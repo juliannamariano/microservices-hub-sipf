2be535a9a9fbd9cfee7e9c3c879d51530475c40e
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-14 11:28:03 -0300

    Add entity pagamento, applicatio-test, update pom, applicatio.properties

---
+++ ms-pagamento/src/main/java/com/github/cidarosa/ms_pagamento/entity/Pagamento.java
@@ -0,0 +1,35 @@
+package com.github.cidarosa.ms_pagamento.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+
+@Entity
+@Table(name = "tb_pagamento")
+public class Pagamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private BigDecimal valor;
+    private String nome;
+    private String numeroDoCarta;
+    private String validde;
+    private String codigoDeSeguranca;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    @Column(nullable = false)
+    private Long pedidoId;
+    @Column(nullable = false)
+    private Long formaDePagamentoId;
+
+}
---
+++ ms-pagamento/src/main/java/com/github/cidarosa/ms_pagamento/entity/Status.java
@@ -0,0 +1,7 @@
+package com.github.cidarosa.ms_pagamento.entity;
+
+public enum Status {
+    CRIADO,
+    CONFIRMADO,
+    CANCELADO
+}
