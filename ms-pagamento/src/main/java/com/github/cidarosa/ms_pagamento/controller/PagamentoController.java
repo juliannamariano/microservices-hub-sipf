b4dbbb6e0e963c590273e823a76f0a054ee38919
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-21 10:39:36 -0300

    feat: add getById em Pagamento Controller

--- ms-pagamento/src/main/java/com/github/cidarosa/ms_pagamento/controller/PagamentoController.java
+++ ms-pagamento/src/main/java/com/github/cidarosa/ms_pagamento/controller/PagamentoController.java
@@ -5,6 +5,7 @@ import com.github.cidarosa.ms_pagamento.service.PagamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,12 @@ public class PagamentoController {
     @GetMapping
     public ResponseEntity<List<PagamentoDTO>> getAll(){
         List<PagamentoDTO> dto = service.getAll();
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PagamentoDTO> getById(@PathVariable Long id){
+        PagamentoDTO dto = service.getById(id);
         return ResponseEntity.ok(dto);
     }
 
