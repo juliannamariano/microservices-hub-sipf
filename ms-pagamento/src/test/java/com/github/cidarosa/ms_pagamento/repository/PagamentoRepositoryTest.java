1b87ad94b446575d0e01688089fd10de6ac2d140
Julianna Mariano <juliannamariano27@outlook.com>
2025-03-28 10:25:40 -0300

    Baixando os testes

--- ms-pagamento/src/test/java/com/github/cidarosa/ms_pagamento/repository/PagamentoRepositoryTest.java
+++ ms-pagamento/src/test/java/com/github/cidarosa/ms_pagamento/repository/PagamentoRepositoryTest.java
@@ -65,5 +65,23 @@ public class PagamentoRepositoryTest {
         Assertions.assertEquals(countTotalPagamento + 1, pagamento.getId());
     }
 
+    @Test
+// @DisplayName("dado um ID existente quando chamar findById então deve retornar um Optional não vazio")
+    @DisplayName("given an existing ID when call findById then return non-empty Optional")
+    public void givenAnExistingId_whenCallFindById_thenReturnNonEmptyOptional() {
+        Optional<Pagamento> result = repository.findById(existingId);
+        Assertions.assertTrue(result.isPresent());
+    }
+
+    @Test
+    @DisplayName("dado um ID Não existente quando chamar findById então deve retornar um Optional vazio")
+    public void givenANonExistingId_whenCallFindById_thenReturnAnEmptyOptional() {
+        Optional<Pagamento> result = repository.findById(nonExistingId);
+        Assertions.assertFalse(result.isPresent());
+        // ou
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+
 
 }
